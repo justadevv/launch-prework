5c9791cc47b37c2cb366ba1d0b42aa8af0b8e824
billy baney <bbaneydev@gmail.com>
2022-06-10 02:29:18 -0500

    finished basic exercises

---
+++ basics/basics.rb
@@ -0,0 +1,37 @@
+# 1. 
+# puts "Billy" + "Baney"
+
+# 2.
+# puts 4936 / 1000
+# puts 4936 % 1000 / 100
+# puts 4936 % 1000 % 100 / 10
+# puts 4936 % 1000 % 100 % 10
+
+# 3. 
+# movies = {
+#     movie1: 1984,
+#     movie2: 1999,
+#     movie3: 1958,
+#     movie4: 2020
+# }
+
+# # movies.each do |k, v|
+# #     puts movies[k]
+# # end
+
+# # 4. 
+# dates = [1984, 1999, 1958, 2020]
+# puts dates
+
+# 5.
+# 5 * 4 * 3 * 2 * 1
+# 6 * 5 * 4 * 3 * 2 * 1
+# 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+# 6.
+# 5.0 ** 2
+# 6.0 ** 2
+# 7.0 ** 2
+
+# 7.
+# Error message tells you that you've accidentally used a ")" and you should have used "}"
